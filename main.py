9810ab966ddaf590d65ec914775651dbef87e2f8
elonmasai <elonmasai7@gmail.com>
2023-02-09 17:02:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print(57*96)
+print("Elon masai")
+num1=78
+num2=32
+print(num1*num2)
+print(num1/num2)
+print(num1+num2)
+# var "Elon masai"
+myname="Elon masai"
+# Dictionry
+mycars={
+    
+}
---
+++ simple calculator.py
@@ -0,0 +1,26 @@
+
+#This is a simple calculator program
+
+print("Welcome to the calculator")
+
+#Get the user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#Calculate the sum
+sum = num1 + num2
+
+#Calculate the difference
+diff = num1 - num2
+
+#Calculate the product
+prod = num1 * num2
+
+#Calculate the Quotient
+quot = num1 / num2
+
+#Print the results
+print("The sum of the two numbers is:", sum)
+print("The difference of the two numbers is:", diff)
+print("The product of the two numbers is:", prod)
+print("The quotient of the two numbers is:", quote)
